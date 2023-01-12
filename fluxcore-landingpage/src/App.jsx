53c8c0a6d1dcd78000292472035efd6f9dcae5f9
Artur <arturrodriguesmota@hotmail.com>
2023-01-11 22:14:43 -0300

    feat: adicionado background

--- fluxcore-landingpage/src/App.jsx
+++ fluxcore-landingpage/src/App.jsx
@@ -1,8 +1,16 @@
 import Header from "./components/Header"
+import Background from "./components/Background";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <>
       <Header/>
+      <Router>
+        <Routes>
+          <Route exact path='/x' element={<Background/>}></Route>
+        </Routes>
+      </Router>
+      <Background/>
     </>
   )
 }
--- fluxcore-landingpage/src/components/Header/index.jsx
+++ fluxcore-landingpage/src/components/Header/index.jsx
@@ -1,18 +1,18 @@
-import styles from './styles.css'
+import styles from './styles.module.css'
 function Header() {
   return (
-    <header className='headerWrapper'>
-      <section className='sectionHeader'>
-        <div className='logoDiv'>
+    <header className={styles.headerWrapper}>
+      <section className={styles.sectionHeader}>
+        <div className={styles.logoDiv}>
           <img src='../public/iconLogo.svg'/>
           <h1>FLUXCORE</h1>
         </div>
-        <navbar className='navbarHeaderWrapper'>
+        <navbar className={styles.navbarHeaderWrapper}>
           <img src='../public/homeIcon.svg'/>
           <h2>NOSSO TRABALHO</h2>
           <h2>SOBRE NÓS</h2>
           <h2>QUEM SOMOS</h2>
-          <h2 className='faleConoscoNavbar'>FALE CONOSCO</h2>
+          <h2 className={styles.faleConoscoNavbar}>FALE CONOSCO</h2>
         </navbar>
       </section>
     </header>
