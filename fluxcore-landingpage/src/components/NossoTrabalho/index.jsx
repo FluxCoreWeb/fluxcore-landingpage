71a26ba096e802ab169e067e3a041893e2cb3bfa
Artur <arturrodriguesmota@hotmail.com>
2023-01-12 12:08:03 -0300

    feat: adicionando card estático

--- fluxcore-landingpage/src/components/NossoTrabalho/index.jsx
+++ fluxcore-landingpage/src/components/NossoTrabalho/index.jsx
@@ -15,6 +15,38 @@ function NossoTrabalho() {
             <img className={styles.imgNossoTrabalho} src='../public/bgNossoTrabalho.svg'/>
           </div>
         </section>
+        <div className={styles.bgProcess}>
+          <section className={styles.processoWrapper}>
+            <h1 className={styles.titleProcess}>Entenda o processo:</h1>
+            <div className={styles.cardsDiv}>
+              <section className={styles.cardDestaque}>
+                <div className={styles.titleCard}>
+                  <h1>1. Contato</h1>
+                  <img src='../public/contactIcon.svg'/>
+                </div>
+                <p className={styles.subtitleCard}>
+                Seja você que fale conosco ou nós que vamos até você, o primeiro passo é sempre muito importante: 
+                nele conheceremos você melhor e entenderemos o que deseja. Por isso, é muito importante que você nos 
+                deixe a par tanto das informações fundamentais do projeto quanto das suas expectativas para o nosso 
+                trabalho juntos. Não se preocupe, nós guiaremos esse contato e estaremos sempre disponíveis para dúvidas 
+                e colocações.
+                </p>
+              </section>
+              <div className={styles.fundoEmb}>
+                <h1 className={styles.titleFundoEmb}>
+                  6 - Lorem ipsum
+                </h1>
+                <p className={styles.subtitleFundoEmb}>Lorem ipsum dolor sit amet consectetur. Nisl ac eu leo a nulla. Justo euismod 
+                  ornare fringilla ornare. Pretium aliquam mauris aliquam quis aliquam tempor lacus ac. 
+                  Sed faucibus vel iaculis accumsan imperdiet suspendisse. Mauris viverra egestas laoreet
+                  vitae amet sed urna facilisi. Sapien elit felis commodo nascetur urna. Nunc in nunc orci 
+                  nisl tincidunt. Vitae ultricies eu viverra purus suscipit. Varius urna at fermentum sed.
+                  Lorem ipsum dolor sit amet consectetur. Nisl ac eu leo a nulla. Justo euismod 
+                  ornare fringilla ornare.</p>
+                </div>
+            </div>
+          </section>
+        </div>
     </main>
   )
 }
